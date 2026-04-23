1e5e1232fc5ca9cf0558a050f713417c758b08ef
Lakhanpatidarr <lp6669349@gmail.com>
2026-04-23 21:43:31 +0530

    fix profile update error + validation

--- MinorServer/controllers/Auth.js
+++ MinorServer/controllers/Auth.js
@@ -7,42 +7,42 @@ const Profile = require("../models/Profile");
 require("dotenv").config();
 const mailSender = require("../utils/mailSender");
 
-exports.sendOTP = async(req,res)=> {
+exports.sendOTP = async (req, res) => {
     try {
-        const {email} = req.body;
-        const checkUserPresent = await User.findOne({email});
-        if(checkUserPresent) {
+        const { email } = req.body;
+        const checkUserPresent = await User.findOne({ email });
+        if (checkUserPresent) {
             return res.status(401).json({
-                success:false,
-                message:"User Already Registered",
+                success: false,
+                message: "User Already Registered",
             });
         }
-        var otp = otpGenerator.generate(6,{
-            upperCaseAlphabets:false,
-            lowerCaseAlphabets:false,
-            specialChars:false,
+        var otp = otpGenerator.generate(6, {
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false,
         });
-        let result = await Otp.findOne({otp:otp});
-        while(result) {
-            otp = otpGenerator.generate(6,{
-                upperCaseAlphabets:false,
-                lowerCaseAlphabets:false,
-                specialChars:false,
+        let result = await Otp.findOne({ otp: otp });
+        while (result) {
+            otp = otpGenerator.generate(6, {
+                upperCaseAlphabets: false,
+                lowerCaseAlphabets: false,
+                specialChars: false,
             })
-            result = await Otp.findOne({otp:otp});
+            result = await Otp.findOne({ otp: otp });
         }
-        const otpPayload = {email,otp};
+        const otpPayload = { email, otp };
         const otpBody = await Otp.create(otpPayload);
         return res.status(200).json({
-            success:true,
-            message:"Otp Send Successfully",
+            success: true,
+            message: "Otp Send Successfully",
         });
     }
-    catch(error) {
+    catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:error.message,
+            success: false,
+            message: error.message,
         });
     }
 };
@@ -50,66 +50,61 @@ exports.sendOTP = async(req,res)=> {
 
 
 
-exports.signUp = async(req,res) => {
+exports.signUp = async (req, res) => {
     try {
-        const{fullname,email,password,confirmPassword,dob,select,otp} = req.body;
-        if(!fullname || !email || !password || !confirmPassword || !otp)
-        {
+        const { fullname, email, password, confirmPassword, dob, select, otp } = req.body;
+        if (!fullname || !email || !password || !confirmPassword || !otp) {
             return res.status(403).json({
-                success:false,
-                message:"All Fields Are Required"
+                success: false,
+                message: "All Fields Are Required"
             });
         }
-        if(password!==confirmPassword)
-        {
+        if (password !== confirmPassword) {
             return res.status(400).json({
-                success:false,
-                message:"Password And ConfirmPassword Does Not Match, Please Try Again",
+                success: false,
+                message: "Password And ConfirmPassword Does Not Match, Please Try Again",
             });
         }
-        const existingUser = await User.findOne({email});
-        if(existingUser)
-        {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
             return res.status(400).json({
-                success:false,
-                message:"User Already Registered",
+                success: false,
+                message: "User Already Registered",
             });
         }
-        const recentOtp = await Otp.find({email}).sort({createdAt:-1}).limit(1);
-        if(recentOtp.length===0)
-        {
+        const recentOtp = await Otp.find({ email }).sort({ createdAt: -1 }).limit(1);
+        if (recentOtp.length === 0) {
             return res.status(400).json({
-                success:false,
-                message:"Otp Not Found",
+                success: false,
+                message: "Otp Not Found",
             });
         }
         const latestOtp = recentOtp[0];
-        if(otp!=latestOtp.otp)
-        {
+        if (otp != latestOtp.otp) {
             return res.status(400).json({
-                success:false,
-                message:"Invalid Otp"
+                success: false,
+                message: "Invalid Otp"
             });
         }
-        const hashedPassword = await bcrypt.hash(password,10);
+        const hashedPassword = await bcrypt.hash(password, 10);
         const profileDetails = await Profile.create({
-            fullname:null,
-            email:null,
-            phoneno:null,
-            address:null
+            fullname: null,
+            email: null,
+            phoneno: null,
+            address: null
         });
-        const user = await User.create({fullname,email,password:hashedPassword,accountType:"Public",additionalDetails:profileDetails._id,dob,select,image:`https://api.dicebear.com/5.x/initials/svg?seed=${fullname}`,});
+        const user = await User.create({ fullname, email, password: hashedPassword, accountType: "Public", additionalDetails: profileDetails._id, dob, select, image: `https://api.dicebear.com/5.x/initials/svg?seed=${fullname}`, department: undefined });
         return res.status(201).json({
-            success:true,
-            message:"User Is Registered Successfully",
+            success: true,
+            message: "User Is Registered Successfully",
             user,
         });
     }
-    catch(error) {
+    catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:'User cannot be registered. Please try again',
+            success: false,
+            message: 'User cannot be registered. Please try again',
         });
     }
 }
@@ -118,120 +113,121 @@ exports.signUp = async(req,res) => {
 
 
 
-exports.login = async(req,res)=> {
+exports.login = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        if(!email || !password)
-        {
+        const { email, password } = req.body;
+        if (!email || !password) {
             return res.status(400).json({
-                success:false,
-                message:"All Fields Are Required, Please Try Again",
+                success: false,
+                message: "All Fields Are Required, Please Try Again",
             });
         }
-        const user = await User.findOne({email}).populate("additionalDetails");
-        if(!user)
-        {
+        const user = await User.findOne({ email }).populate("additionalDetails");
+        if (!user) {
             return res.status(401).json({
-                success:false,
-                message:"User Is Not Registered, Please SignUp First",
+                success: false,
+                message: "User Is Not Registered, Please SignUp First",
             });
         }
-        if(await bcrypt.compare(password,user.password))
-        {
+        if (await bcrypt.compare(password, user.password)) {
             const payload = {
-                email:user.email,
-                id:user._id,
-                accountType:user.accountType,
-                department:user.department
+                email: user.email,
+                id: user._id,
+                accountType: user.accountType,
+                department: user.department
             }
-            const token = jwt.sign(payload,process.env.JWT_SECRET,{
-                expiresIn:"2h",
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "2h",
             });
             user.token = token;
             user.password = undefined;
             const options = {
-                expires:new Date(Date.now()+3*24*60*60*1000),
-                httpOnly:true,
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
             }
-            res.cookie("token",token,options).status(200).json({
-                success:true,
+            res.cookie("token", token, options).status(200).json({
+                success: true,
                 token,
                 user,
-                message:"Logged In Successfully",
+                message: "Logged In Successfully",
             });
         }
         else {
             return res.status(401).json({
-                success:false,
-                message:"Password Incorrect",
+                success: false,
+                message: "Password Incorrect",
             });
         }
     }
-    catch(error) {
+    catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Login Failure Please Try Again",
+            success: false,
+            message: "Login Failure Please Try Again",
         });
     }
 };
 
 
 
-exports.changePassword = async(req,res)=>{
+exports.changePassword = async (req, res) => {
     try {
-        const {email,password,newPassword,confirmNewPassword} = req.body;
-        console.log("BODY:", req.body);
-        if(!password || !newPassword || !confirmNewPassword)
-        {
+        const { email, password, newPassword, confirmNewPassword } = req.body;
+        if (!password || !newPassword || !confirmNewPassword) {
             return res.status(400).json({
-                success:false,
-                message:"Please Filled All The Details Carefully",
+                success: false,
+                message: "Please Filled All The Details Carefully",
             });
         }
-        const user = await User.findOne({email});
-        console.log("USER:", user);
-        if(!user)
-        {
+        const user = await User.findOne({ email });
+        if (!user) {
             return res.status(400).json({
-                success:false,
-                message:"User Not Found",
+                success: false,
+                message: "User Not Found",
             });
         }
         if (!user.password) {
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            user.password = hashedPassword;
+            user.department = undefined;
+            await user.save({ validateBeforeSave: false });
+            return res.status(200).json({
+                success: true,
+                message: "Password set successfully"
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(400).json({
                 success: false,
-                message: "Google account detected. Please set password first."
+                message: "Old Password Is Incorrect",
             });
         }
-        const isMatch = await bcrypt.compare(password,user.password);
-        if(!isMatch) {
+        if (newPassword != confirmNewPassword) {
             return res.status(400).json({
-                success:false,
-                message:"Old Password Is Incorrect",
+                success: false,
+                message: "New Password Does Not Match Confirm Password",
             });
         }
-        if(newPassword!=confirmNewPassword)
-        {
-            return res.status(400).json({
-                success:false,
-                message:"New Password Does Not Match Confirm Password",
-            });
-        }
-        const hashedPassword = await bcrypt.hash(newPassword,10);
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
         user.password = hashedPassword;
-        await user.save();
-        await mailSender(user.email,"Password Updated Successfully","Your password has been changed successfully. If you did not perform this action, please contact support immediately.");
+        if (user.accountType === "Public" || user.accountType === "SuperAdmin") {
+            await User.updateOne(
+                { _id: user._id },
+                { $unset: { department: "" } }
+            );
+        }
+        await mailSender(user.email, "Password Updated Successfully", "Your password has been changed successfully. If you did not perform this action, please contact support immediately.");
         return res.status(200).json({
-            success:true,
-            message:"Password Change Successfully",
+            success: true,
+            message: "Password Change Successfully",
         });
     }
-    catch(error) {
+    catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Can't Change The Password, Please Try Again",
+            success: false,
+            message: "Can't Change The Password, Please Try Again",
         });
     }
 }
--- MinorServer/controllers/Profile.js
+++ MinorServer/controllers/Profile.js
@@ -2,80 +2,99 @@ const Profile = require("../models/Profile");
 const User = require("../models/User");
 
 
-exports.updateProfile = async(req,res)=> {
+exports.updateProfile = async (req, res) => {
     try {
-        const {fullname,email,phoneno,address} = req.body;
+        const { fullname, email, phoneno, address } = req.body;
         const id = req.user.id;
-        if(!fullname || !email || !phoneno || !address) {
+        const phone = Number(phoneno);
+        if (isNaN(phone)) {
             return res.status(400).json({
-                success:false,
-                message:"All field require",
+                success: false,
+                message: "Invalid phone number",
+            });
+        }
+        if (!fullname || !email || phoneno === undefined || !address) {
+            return res.status(400).json({
+                success: false,
+                message: "All field require",
             });
         }
         const userDetails = await User.findById(id);
+        if (!userDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
         const profileId = userDetails.additionalDetails;
         const profileDetails = await Profile.findById(profileId);
+        if (!profileDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Profile not found",
+            });
+        }
 
 
         profileDetails.fullname = fullname;
         profileDetails.email = email;
-        profileDetails.phoneno = phoneno;
+        profileDetails.phoneno = phone;
         profileDetails.address = address;
         await profileDetails.save();
         const updatedUser = await User.findById(id).populate("additionalDetails").exec();
         return res.status(200).json({
-            success:true,
-            message:"Profile updated successfully",
-            user:updatedUser,
+            success: true,
+            message: "Profile updated successfully",
+            user: updatedUser,
         });
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Error while update profile",
+            success: false,
+            message: "Error while update profile",
         })
     }
 };
 
-exports.deleteAccount = async(req,res)=> {
+exports.deleteAccount = async (req, res) => {
     try {
         const id = req.user.id;
         const userDetails = await User.findById(id);
-        if(!userDetails) {
+        if (!userDetails) {
             return res.status(404).json({
-                success:false,
-                message:"User not found",
+                success: false,
+                message: "User not found",
             });
         }
-        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
-        await User.findByIdAndDelete({_id:id});
+        await Profile.findByIdAndDelete({ _id: userDetails.additionalDetails });
+        await User.findByIdAndDelete({ _id: id });
         return res.status(200).json({
-            success:true,
-            message:"Account delete successfully",
+            success: true,
+            message: "Account delete successfully",
         });
     }
-    catch(error) {
+    catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Error while deleting account",
+            success: false,
+            message: "Error while deleting account",
         });
     }
 };
 
-exports.getAllUserDetails = async(req,res) => {
+exports.getAllUserDetails = async (req, res) => {
     try {
         const id = req.user.id;
         const userDetails = await User.findById(id).populate("additionalDetails").exec();
         return res.status(200).json({
-            success:true,
-            message:"User Data fetch Successfully",
+            success: true,
+            message: "User Data fetch Successfully",
             data: userDetails,
         });
     }
-    catch(error) {
+    catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Error while get all details of user",
+            success: false,
+            message: "Error while get all details of user",
         });
     }
 }
